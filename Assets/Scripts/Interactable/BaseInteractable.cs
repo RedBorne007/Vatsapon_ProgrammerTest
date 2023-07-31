196f2ff610c0d25cd1bd7e5d2a21e942afeab8ef
Vatsapon Asawakittiporn <vatsapon33@gmail.com>
2023-07-31 10:55:40 +0700

    Integrated some prototype assets, Added basic Interact system
    - Integrated some prototype assets.
    - Added basic Interact system

--- Assets/Scripts/Interactable/BaseInteractable.cs
+++ Assets/Scripts/Interactable/BaseInteractable.cs
@@ -9,23 +9,33 @@ public class BaseInteractable : MonoBehaviour
     [SerializeField] private string interactLabel;
 
     [Header("References")]
-    [SerializeField] private GameObject interactCanvas;
-    [SerializeField] private TMP_Text interactText;
+    [SerializeField] private GameObject interactPopupPrefab;
 
     [SerializeField] private UnityEvent onInteract;
 
     protected bool isRaycasted; // Determine if player is raycasting this interactable.
+    private GameObject currentPopup;
 
     protected virtual void Start()
     {
-        interactText.text = interactLabel;
+        currentPopup = Instantiate(interactPopupPrefab);
+        currentPopup.transform.Find("Text").GetComponent<TMP_Text>().text = interactLabel;
     }
 
     protected virtual void Update()
     {
-        interactCanvas.SetActive(isRaycasted);
+        CanvasUpdater();
     }
-    
+
+    // Function to update canvas.
+    private void CanvasUpdater()
+    {
+        currentPopup.SetActive(isRaycasted);
+    }
+
+    // Function to set value if it's being raycasted or not.
+    public void SetRaycasted(bool value) => isRaycasted = value;
+
     // Function to execute when player interact with this interactable.
     public virtual void OnInteract()
     {
--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -4,10 +4,61 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
+    [Tooltip("Determine to use debug mode or not")]
     [SerializeField] private bool isDebug = false;
 
     [Space]
 
     [Header("Settings")]
+    [Tooltip("Layer that affect by player's interaction raycast")]
+    [SerializeField] private LayerMask interactLayer = ~0;
+    [Tooltip("Range of player's interaction")]
     [SerializeField] private float interactRange = 5f;
+
+    private BaseInteractable currentInteractable;
+
+    private void Update()
+    {
+        RaycastHandler();
+    }
+
+    // Function to handle raycast for interaction.
+    private void RaycastHandler()
+    {
+        Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, interactRange, interactLayer);
+
+        // If raycast hit something and has Interactable...
+        if (hit.collider && hit.collider.TryGetComponent(out BaseInteractable interactable))
+        {
+            // If there's current interactable and it's different from new one, replace it.
+            if (currentInteractable && !currentInteractable.Equals(interactable))
+            {
+                currentInteractable.SetRaycasted(false);
+                currentInteractable = interactable;
+                interactable.SetRaycasted(true);
+            }
+
+            // If there's no current interactable yet, set the hit one as the current.
+            if (!currentInteractable)
+            {
+                currentInteractable = interactable;
+                currentInteractable.SetRaycasted(true);
+            }
+        }
+        else
+        {
+            // Else, make it un-raycast (if there's current interactable).
+            currentInteractable?.SetRaycasted(false);
+            currentInteractable = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isDebug)
+        {
+            Gizmos.color = currentInteractable ? Color.green : Color.yellow;
+            Gizmos.DrawLine(transform.position, transform.position + (transform.forward * interactRange));
+        }
+    }
 }
